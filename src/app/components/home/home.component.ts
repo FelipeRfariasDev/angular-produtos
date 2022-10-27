ff7309e370699b7ebea75a9de8afaa451e62d32e
Felipe R Farias <feliperfariasdev@gmail.com>
2022-10-27 07:23:34 -0300

    na tela da home mensagem se o usuario está logado ou não

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit{
   title = 'Seja Bem Vindo';
+  msgUsuarioLogado:any;
 
   ngOnInit():void{
     const accessToken = localStorage.getItem('accessToken');
     if(accessToken){
-      console.log(' Usuário Locado com accessToken: '+accessToken);
+      this.msgUsuarioLogado = ' Usuário logado com accessToken: '+accessToken;
+    }else{
+      this.msgUsuarioLogado = 'Usuário não está logado';
     }
   }
 }
